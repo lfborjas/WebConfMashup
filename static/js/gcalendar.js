6b2f07e113a38c80e8526a13ecd9a5eb18570001
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-07-22 00:12:30 -0600

    inicio de lo de calendar, obteniendo el feed del calendario privado de un usuario

---
+++ static/js/gcalendar.js
@@ -0,0 +1,54 @@
+google.load("gdata", "2.2");
+var calendarService;
+google.setOnLoadCallback(onGoogleDataLoad);
+function onGoogleDataLoad(){
+	var scope = "http://www.google.com/calendar/feeds/"; //could be a list of scopes, for more services 
+	$('#google-login').button();
+	if(!google.accounts.user.checkLogin(scope)){	
+		//bind the login button:		
+		$('#google-login').click(logIn);
+	}else{//is authenticated
+		//bind the logout and show the feed
+		$('#google-login').text("Logout");
+		$('#google-login').click(logOut);
+		getCalendarFeed();
+	}
+	
+	function logIn(){		 
+		 var token = google.accounts.user.login(scope);
+		 //change the button (show logout):
+		 $('#google-login').text("Logout");
+		 $('#google-login').unbind("click");
+		 $('#google-login').click(logOut);
+		 //get the events from the calendar:
+		 getCalendarFeed();
+	}
+	
+	function getCalendarFeed(){
+		calendarService = new google.gdata.calendar.CalendarService('WebConfMashup');
+		//the default private calendar:
+		var feedUri = 'http://www.google.com/calendar/feeds/default/private/full/';
+		calendarService.getEventsFeed(feedUri, 
+		function(result){ //callback function
+			$.each(result.feed.entry, function(index,event){
+				$("#calendar-feed").append("<li>"+event.getTitle().getText()+"</li>");
+			});				
+		}, 
+		function(error){ //for error handling
+			$('#calendar-error').text(error);
+		});
+	}
+	
+	function logOut(){
+		google.accounts.user.logout();
+		$('#google-login').text("Login to calendar");
+		$('#google-login').unbind("click");
+		$('#google-login').click(logIn);
+	}
+	
+	
+	
+	
+}
+
+
