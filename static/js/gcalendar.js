752ac251cd2275292e6635a02cad491fad604668
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-07-22 00:24:37 -0600

    dejando en orden algunas vars globales para la creación de eventos

--- static/js/gcalendar.js
+++ static/js/gcalendar.js
@@ -1,33 +1,36 @@
 google.load("gdata", "2.2");
-var calendarService;
+
 google.setOnLoadCallback(onGoogleDataLoad);
 function onGoogleDataLoad(){
-	var scope = "http://www.google.com/calendar/feeds/"; //could be a list of scopes, for more services 
+	var scope = "http://www.google.com/calendar/feeds/"; //could be a list of scopes, for more services
+	var calendarService = new google.gdata.calendar.CalendarService('WebConfMashup');	
+	//the default private calendar:
+	var feedUri = 'http://www.google.com/calendar/feeds/default/private/full/';
+	
 	$('#google-login').button();
 	if(!google.accounts.user.checkLogin(scope)){	
 		//bind the login button:		
 		$('#google-login').click(logIn);
 	}else{//is authenticated
 		//bind the logout and show the feed
-		$('#google-login').text("Logout");
+		$('#google-login').text("Logout from calendar");
 		$('#google-login').click(logOut);
 		getCalendarFeed();
+		
+		//show the event creation form:
 	}
 	
 	function logIn(){		 
 		 var token = google.accounts.user.login(scope);
 		 //change the button (show logout):
-		 $('#google-login').text("Logout");
+		 $('#google-login').text("Logout from calendar");
 		 $('#google-login').unbind("click");
 		 $('#google-login').click(logOut);
 		 //get the events from the calendar:
 		 getCalendarFeed();
 	}
 	
-	function getCalendarFeed(){
-		calendarService = new google.gdata.calendar.CalendarService('WebConfMashup');
-		//the default private calendar:
-		var feedUri = 'http://www.google.com/calendar/feeds/default/private/full/';
+	function getCalendarFeed(){		
 		calendarService.getEventsFeed(feedUri, 
 		function(result){ //callback function
 			$.each(result.feed.entry, function(index,event){
